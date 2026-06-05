dbf5d35dc9ae14d58c41149bc5bbc7200bc1bbf9
jeffgithae <jeffkinyua64@gmail.com>
2026-06-06 02:10:11 +0300

    web environments

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,4 +1,6 @@
+
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api',
+  production: true,
+  // apiUrl: 'http://localhost:3000/api',
+  apiUrl: 'https://monival-me-production.up.railway.app/api',
 };
